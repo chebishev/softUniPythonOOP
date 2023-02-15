6ed21070f0ceca0eb7ee64a4641c1b456ef63b8b
chebishev <atanas.chebishev@gmail.com>
2023-02-15 13:38:20 +0200

    oop first steps 4/5

--- 20221024_First_Steps_In_OOP/rhombus_of_stars.py
+++ 20221024_First_Steps_In_OOP/rhombus_of_stars.py
@@ -1,5 +1,17 @@
-for _ in range(int(input())):
-    print("*")
+def print_row(figure_size, row_size):
+    print(' ' * (figure_size - row_size), end='')
+    for _ in range(1, row_size + 1):
+        print('* ', end='')
+    print()
+
+
+n = int(input())
+
+for row in range(1, n + 1):
+    print_row(n, row)
+
+for row in range(n - 1, -1, -1):
+    print_row(n, row)
 
 # test inputs:
 
