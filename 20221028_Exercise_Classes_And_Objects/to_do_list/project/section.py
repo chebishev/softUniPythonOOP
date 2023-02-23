d96f60bd8beeac17c952f3b523fdf8a9cb1362bb
chebishev <atanas.chebishev@gmail.com>
2023-02-23 15:56:31 +0200

    Exercise: Classes and Objects 5/8

--- 20221028_Exercise_Classes_And_Objects/to_do_list/project/section.py
+++ 20221028_Exercise_Classes_And_Objects/to_do_list/project/section.py
@@ -9,22 +9,34 @@ class Section:
     def add_task(self, new_task: Task):
         if new_task in self.tasks:
             return f"Task is already in the section {self.name}"
+
         self.tasks.append(new_task)
-        return f"Task {self.name.task.details()} is added to the section"
+
+        return f"Task {new_task.details()} is added to the section"
 
     def complete_task(self, task_name):
-        if task_name in self.tasks:
-            pass
+        try:
+            found_task = next(filter(lambda t: t.name == task_name, self.tasks))
+
+        except StopIteration:
+            return f"Could not find task with the name {task_name}"
+
+        found_task.completed = True
+        return f"Completed task {task_name}"
 
     def clean_section(self):
-        cleared_tasks = len(self.tasks)
-        self.tasks = []
+        cleared_tasks = 0
+
+        for current_task in filter(lambda t: t.completed, self.tasks):
+            self.tasks.remove(current_task)
+            cleared_tasks += 1
+
         return f"Cleared {cleared_tasks} tasks."
 
     def view_section(self):
         output = [f'Section {self.name}:']
-        for current_task in self.tasks:
-            output.append(current_task)
+        [output.append(t.details()) for t in self.tasks]
+
         return '\n'.join(output)
 
 
