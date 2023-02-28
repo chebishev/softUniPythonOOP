999f2867206f950b6a471a61d0aace8294194ea0
chebishev <atanas.chebishev@gmail.com>
2023-02-28 14:16:56 +0200

    Exercises: Inheritance 4/5

---
+++ 20221101_Exercise_Inheritance/zoo/project/snake.py
@@ -0,0 +1,15 @@
+from project.reptile import Reptile
+from project.mammal import Mammal
+from project.lizard import Lizard
+
+
+class Snake(Reptile):
+    pass
+
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
