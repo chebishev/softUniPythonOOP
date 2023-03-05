d94cca70f924428d8590dd1f38b60c9140cce445
chebishev <atanas.chebishev@gmail.com>
2023-03-05 15:04:45 +0200

    Exercises: Inheritance - Completed

---
+++ 20230301_Exercise_Inheritance/shop/project/product_repository.py
@@ -0,0 +1,36 @@
+from project.drink import Drink
+from project.food import Food
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        result = self.find(product_name)
+
+        if result:
+            self.products.remove(result)
+
+    def __repr__(self):
+        return '\n'.join(f"{product}: {product.quantity}" for product in self.products)
+
+
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)
---
+++ 20230301_Exercise_Inheritance/zoo/project/bear.py
@@ -0,0 +1,14 @@
+from project.lizard import Lizard
+from project.mammal import Mammal
+
+
+class Bear(Mammal):
+    pass
+
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
