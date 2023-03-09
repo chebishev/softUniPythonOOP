47cc8f61698a1720ea87b930195076c276aeafaf
chebishev <atanas.chebishev@gmail.com>
2023-03-09 09:42:03 +0200

    Exercise: Encapsulation - Completed

--- 20230306_Exercise_Encapsulation/restaurant/project/beverage/coffee.py
+++ 20230306_Exercise_Encapsulation/restaurant/project/beverage/coffee.py
@@ -5,7 +5,11 @@ class Coffee(HotBeverage):
     MILLILITERS = 50
     PRICE = 3.50
 
-    def __init__(self, name, price, milliliters, caffeine):
-        super().__init__(name, price, milliliters)
+    def __init__(self, name, caffeine):
+        super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
         self.__caffeine = caffeine
+
+    @property
+    def caffeine(self):
+        return self.__caffeine
 
