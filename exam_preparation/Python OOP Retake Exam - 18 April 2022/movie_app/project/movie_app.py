a8853800bd1c50f1161bd4bcee50abf267d67368
chebishev <atanas.chebishev@gmail.com>
2023-04-02 16:40:12 +0300

    Python OOP Retake Exam - 18 April 2022 - 281/300

--- exam_preparation/Python OOP Retake Exam - 18 April 2022/movie_app/project/movie_app.py
+++ exam_preparation/Python OOP Retake Exam - 18 April 2022/movie_app/project/movie_app.py
@@ -7,6 +7,7 @@ class MovieApp:
     def __init__(self):
         self.movies_collection = []
         self.users_collection = []
+        self.movies = []
 
     def get_user(self, username) -> List[User]:
         return [u for u in self.users_collection if u.username == username]
@@ -19,27 +20,30 @@ class MovieApp:
             raise Exception(f"The movie {movie.title} is not uploaded!")
 
     def register_user(self, username, age) -> str:
-        try:
-            user = [u for u in self.users_collection if u.username == username][0]
-            raise Exception("User already exists!")
-        except IndexError:
-            user = User(username, age)
-            self.users_collection.append(user)
+        user = User(username, age)
+        for u in self.users_collection:
+            if u.username == username:
+                raise Exception("User already exists!")
 
-            return f"{username} registered successfully."
+        self.users_collection.append(user)
+
+        return f"{username} registered successfully."
 
     def upload_movie(self, username, movie) -> str:
 
-        try:
-            user = [u for u in self.users_collection if u.username == username][0]
-        except IndexError:
+        for user in self.users_collection:
+            if user.username == username:
+                break
+        else:
             raise Exception("This user does not exist!")
 
+        user = self.get_user(username)[0]
         if not movie.owner == user:
             raise Exception(f"{username} is not the owner of the movie {movie.title}!")
 
-        if movie in self.movies_collection:
-            raise Exception("Movie already added to the collection!")
+        for m in self.movies_collection:
+            if m.title == movie.title:
+                raise Exception("Movie already added to the collection!")
 
         user.movies_owned.append(movie)
         self.movies_collection.append(movie)
