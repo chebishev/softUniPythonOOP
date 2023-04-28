8a20aa8788f5286ad741d750a4518dde815c7faa
chebishev <atanas.chebishev@gmail.com>
2023-04-28 14:09:43 +0300

    Python OOP Exam - 10 April 2021

---
+++ exam_preparation/Python OOP Exam - 10 April 2021/aqua_shop/project/decoration/decoration_repository.py
@@ -0,0 +1,17 @@
+class DecorationRepository:
+    def __init__(self):
+        self.decorations = []
+
+    def add(self, decoration):
+        self.decorations.append(decoration)
+
+    def remove(self, decoration):
+        if decoration not in self.decorations:
+            return False
+        self.decorations.remove(decoration)
+        return True
+
+    def find_by_type(self, decoration_type):
+        if not [decoration for decoration in self.decorations if decoration.__class__.__name__ == decoration_type]:
+            return None
+        return [decoration for decoration in self.decorations if decoration.__class__.__name__ == decoration_type][0]
